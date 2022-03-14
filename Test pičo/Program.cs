d899945791d614fd59447749732e87fe5b6be72c
Jan Gergel <jan.gergel@educhem.cz>
2022-03-14 09:08:03 +0100

    Přidejte soubory projektu.

---
+++ Test pičo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more informationConsole.WriteLine("Hello, World!");
+List<double> seznam = new List<double>();
+seznam.Add(0.77);
+seznam.Add(150.24);
+seznam.Add(125.98);
+seznam.Add(50.65);
+seznam.Add(53.68);
+foreach (double hodnota in seznam)
+{
+    if (hodnota > 100)
+    {
+        Console.WriteLine(hodnota);
+    }
+}
+
+
+
+
+
